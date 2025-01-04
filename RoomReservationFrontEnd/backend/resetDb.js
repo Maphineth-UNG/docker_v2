71c6435da50e287f50e48539eedc1050aa1eb940
maphineth <maphinethung@gmail.com>
2025-01-04 16:06:23 +0100

    deleted file

--- RoomReservationFrontEnd/backend/resetDb.js
+++
@@ -1,7 +0,0 @@
-const { deleteAndRecreateFileDatabase, createTables, populateDatase } = require('./dbUtil.js');
-
-const db = deleteAndRecreateFileDatabase();
-createTables(db);
-populateDatase(db);
-db.close();
-console.log('Database reset complete.');
